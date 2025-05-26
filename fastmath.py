b0891384cc7ec0718cc32b86a58d5924dc2833d2
Breno Martins <crdj161@gmail.com>
2025-05-26 14:04:44 -0300

    Consertado a função de checar se é numero e unido codigos semelhantes

--- fastmath.py
+++ fastmath.py
@@ -15,14 +15,20 @@ def is_number(s: str) -> bool:
     if s.isdigit():
         return True
 
-    # Verifica se é um float com vírgula (ex: "3,14")
-    if "," in s:
-        partes = s.split(",")
-        return len(partes) == 2 and all(p.isdigit() for p in partes)
+    # Verifica se é um float com vírgula ou ponto (ex: "3,14")
+    separators = [",", ".", "/"]
+    for sep in separators:
+        if sep in s:
+            parts = s.split(sep)
+            return len(parts) == 2 and all(p.isdigit() for p in parts)
+    
+    #if "," in s:
+    #    partes = s.split(",")
+    #    return len(partes) == 2 and all(p.isdigit() for p in partes)
 
     # Verifica se é uma fração com barra (ex: "3/4")
-    if "/" in s:
-        partes = s.split("/")
-        return len(partes) == 2 and all(p.isdigit() for p in partes)
+    #if "/" in s:
+    #    partes = s.split("/")
+    #    return len(partes) == 2 and all(p.isdigit() for p in partes)
 
     return False
